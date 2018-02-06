cce9e367ea6dff7dc41e77db5fd5d6b9ac0c83c7
Donnie Nowels <djnowels11@gmail.com>
2018-02-06 11:18:38 -0500

    deleted cytoscape.js folder

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -12,8 +12,6 @@ import Foundation from 'foundation-sites';
 $(document).foundation().ready( function(){
   $("#getdata-button").click(function() {
     console.log("reading read_data!!");
-    $.getJSON("../assets/json/dealers.json", function(data) {
-      document.getElementById("")
 
 
 
@@ -21,35 +19,46 @@ $(document).foundation().ready( function(){
 
 
 
-
-
-
-        //individual dealer array
-      var dealers = data.dealers;
-      console.log(data);
-      console.log("this is the dealers array " + dealers);
-      console.log(dealers[1]);
-      var certs =[dealers[1].data.certifications];
-      $.each(certs, function(i, val){
-        console.log("i issss " + i);
-        console.log("val isss " + val);
-        var value = val[i];
-        console.log("This is the Value" + value)
-        console.log("Week hours " + dealers[1].data.weekHours.mon)
-        if(value == "Installation Pro"){
-          $(".company_list").append("<h2 class='company-name'>" + dealers[1].data.name + "<h2>");
-          $(".company_list").append("<p class='phone'>" + dealers[1].data.phone1 + "<p>");
-          $(".company_list").append("<p class='click-below'><i>Can't talk now? Click below to send an email.<i><p>");
-          $(".company_list").append("<a class='contact' href="+dealers[1].data.email+">Contact this pro</a>");
-          $(".company_list").append("<p class='hours'><strong>Business Hours</strong></p>");
-          $(".company_list").append("<p class='weekdays'>Weekdays "  + dealers[1].data.weekHours.mon + "</p>");
-          $(".company_list").append("<p class='saturday'>Saturdays "  + dealers[1].data.weekHours.sat + "</p>");
-          $(".company_list").append("<p class='sunday'>Sundays "  + dealers[1].data.weekHours.sun + "</p>");
-        }else{
-          alert("this blows");
-        }
-      });
-    });
+    // $.getJSON("../dealers.json", function(data) {
+    //   // document.getElementById("companies").innerHTML = data.dealers[1]["name"]
+    //   // var source   = document.getElementById('text-template').innerHTML;
+    //   // var template = Handlebars.compile(source);
+    //   // var html    = template(data);
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //     //individual dealer array
+    //   var dealers = data.dealers;
+    //   console.log(data);
+    //   console.log(data.dealers[1]);
+    //   console.log(dealers[1]);
+    //   var certs =[dealers[1].data.certifications];
+    //   $.each(certs, function(i, val){
+    //     console.log("i issss " + i);
+    //     console.log("val isss " + val);
+    //     var value = val[i];
+    //     console.log("This is the Value" + value)
+    //     console.log("Week hours " + dealers[1].data.weekHours.mon)
+    //     if(value == "Installation Pro"){
+    //       $(".company_list").append("<h2 class='company-name'>" + dealers[1].data.name + "<h2>");
+    //       $(".company_list").append("<p class='phone'>" + dealers[1].data.phone1 + "<p>");
+    //       $(".company_list").append("<p class='click-below'><i>Can't talk now? Click below to send an email.<i><p>");
+    //       $(".company_list").append("<a class='contact' href="+dealers[1].data.email+">Contact this pro</a>");
+    //       $(".company_list").append("<p class='hours'><strong>Business Hours</strong></p>");
+    //       $(".company_list").append("<p class='weekdays'>Weekdays "  + dealers[1].data.weekHours.mon + "</p>");
+    //       $(".company_list").append("<p class='saturday'>Saturdays "  + dealers[1].data.weekHours.sat + "</p>");
+    //       $(".company_list").append("<p class='sunday'>Sundays "  + dealers[1].data.weekHours.sun + "</p>");
+    //     }else{
+    //       alert("this blows");
+    //     }
+    //   });
+    // });
   });
 });
 
